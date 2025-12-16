9904cc8bc0d0706471dced706557fc932322606a
GabuWaSaikou <contact.saikou.games@gmail.com>
2025-12-16 17:44:20 +0100

    FEAT: create a basic config.cfg for R1
    Need to rename the file in order to use it
    
    The string is parsed raw in the .go script, which looks dirty.

--- main.go
+++ main.go
@@ -1,8 +1,60 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+)
+
+func writeConfig(router_name string) {
+    filename := "config" + ".cfg"
+
+    file, err := os.Create(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+	header := `!
+!
+!
+service timestamps debug datetime msec
+service timestamps log datetime msec
+no service password-encryption
+!
+hostname`
+
+	tail := `!
+ip cef
+no ip domain-lookup
+no ip icmp rate-limit unreachable
+ip tcp synwait 5
+no cdp log mismatch duplex
+!
+line con 0
+exec-timeout 0 0
+logging synchronous
+privilege level 15
+no login
+line aux 0
+exec-timeout 0 0
+logging synchronous
+privilege level 15
+no login
+!
+!
+end`
+
+	content := fmt.Sprintf("%s %s\n%s", header, router_name, tail)
+	
+    _, err = file.WriteString(content)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(".cfg écrit sous", filename)
+}
 
 func main() {
-
-	fmt.Println("Hello World !")
+    writeConfig("R1")
 }
