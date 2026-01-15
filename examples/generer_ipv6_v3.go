00d12088e504cd7e920661a6e473dec5fb3ff3f0
Nanyi <18167108330@163.com>
2026-01-15 09:55:45 +0100

    correction & ipv6configuration -> fichier.cfg

--- examples/generer_ipv6_v3.go
+++ examples/generer_ipv6_v3.go
@@ -9,12 +9,18 @@ import (
 
 func generateIPv6(prefix string, subnetID int, ifaceID int) string {
 	base := strings.TrimSuffix(prefix, "::")
-	return fmt.Sprintf("%s:%d::%d", base, subnetID, ifaceID)
+	return fmt.Sprintf("%s:%d::%d/64", base, subnetID, ifaceID)
+}
+
+func generateLoopback(prefix string, routerName string) string {
+	base := strings.TrimSuffix(prefix, "::")
+	id := routerName[1:]
+	return fmt.Sprintf("%s:100::%s/128", base, id)
 }
 
 func main() {
 	// lecture du intent
-	data, err := os.ReadFile("examples/json/network_intent_new.json")
+	data, err := os.ReadFile("json/network_intent_template_v3.json")
 	if err != nil {
 		panic(err)
 	}
@@ -27,11 +33,23 @@ func main() {
 	// parcourrir l'AS
 	for asName, asData := range intent {
 		fmt.Println("AS:", asName)
+		cfg := make(map[string]string)
 
 		asMap := asData.(map[string]interface{})
 		prefix := asMap["network_subnet"].(string)
 
 		links := asMap["links"].([]interface{})
+		routers := asMap["routers"].(map[string]interface{})
+
+		// loopback ipv6
+		for routerName := range routers {
+			cfg[routerName] = "ipv6 unicast-routing\n\n"
+			loopbackIPv6 := generateLoopback(prefix, routerName)
+			cfg[routerName] += fmt.Sprintf(
+				"interface Loopback0\n ipv6 address %s\n\n",
+				loopbackIPv6,
+			)
+		}
 
 		// links
 		for _, linkRaw := range links {
@@ -51,13 +69,22 @@ func main() {
 
 				ipv6 := generateIPv6(prefix, subnetID, ifaceID)
 
-				fmt.Printf(
-					"Router: %s  Interface: %s  Subnet: %d  IPv6: %s\n",
-					router, iface, subnetID, ipv6,
+				cfg[router] += fmt.Sprintf(
+					"interface %s\n ipv6 address %s\n ipv6 rip RIPng enable\n no shutdown\n\n",
+					iface, ipv6,
 				)
 
 				ifaceID++
 			}
+		}
+
+		for routerName, content := range cfg {
+			filename := routerName + ".cfg"
+			err := os.WriteFile(filename, []byte(content), 0644)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Generated", filename)
 		}
 	}
 }
