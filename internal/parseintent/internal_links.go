49d0c25d03a42835f979beddf06e259cc7c86262
Nanyi <18167108330@163.com>
2026-01-19 16:56:21 +0100

    traitement des liens internals

---
+++ internal/parseintent/internal_links.go
@@ -0,0 +1,53 @@
+package parseintent
+
+import (
+	"strconv"
+)
+
+type InterfacePlan struct {
+	Name      string
+	IPv6      string
+	EnableIGP bool
+}
+
+func planInternalLinks(
+	links []interface{},
+	prefix string,
+) map[string][]InterfacePlan {
+
+	res := make(map[string][]InterfacePlan)
+
+	for _, l := range links {
+		link := l.(map[string]interface{})
+
+		role, ok := link["role"].(string)
+		if !ok || role != "internal" {
+			// traiter seulement les interfaces "internal"
+			continue
+		}
+
+		info := link["info"].(map[string]interface{})
+
+		subnetStr := info["subnet"].(string)
+		subnetID, _ := strconv.Atoi(subnetStr)
+
+		endpoints := link["endpoints"].(map[string]interface{})
+
+		ifaceID := 1
+		for router, ifaceRaw := range endpoints {
+			iface := ifaceRaw.(string)
+
+			ipv6 := generateIPv6(prefix, subnetID, ifaceID)
+
+			res[router] = append(res[router], InterfacePlan{
+				Name:      iface,
+				IPv6:      ipv6,
+				EnableIGP: true,
+			})
+
+			ifaceID++
+		}
+	}
+
+	return res
+}
