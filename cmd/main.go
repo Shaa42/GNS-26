363cfd76b24a504e37ddb3ed5ff851d1da348c7b
Shaa42 <ryanvigoureux@hotmail.com>
2026-01-27 11:20:22 +0100

    FEAT: Added new network

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"gns-26/applycfg"
+	"gns-26/internal/applycfg"
 	"gns-26/internal/parseintent"
 	"gns-26/internal/writeconf"
 )
 
 func main() {
 
-	// Parse json intent file and print the as' info
-	asMap, err := parseintent.NewAS("examples/json/network_intent_template_v4.json")
+	// Parse json intent file and print the AS' info
+	asMap, err := parseintent.NewAS("networks/network_intent_2.json")
 	if err != nil {
 		panic(err)
 	}
-	as111 := asMap["AS111"]
-	as112 := asMap["AS112"]
-	// Create a .cfg file for router named R1 with data dict
-	writeconf.WriteConfig(as111)
-	as111.LogAS()
-	writeconf.WriteConfig(as112)
-	as112.LogAS()
 
-	// Apply ths just created .cfg to every router
-	for _, router := range as111.Routers {
-		applycfg.ApplyCfg(router.Name[1:], "GNS-project")
+	for _, asVal := range asMap {
+		// Create a .cfg file for every AS' router
+		writeconf.WriteConfig(asVal)
+		// asVal.LogAS()
+		for _, router := range asVal.Routers {
+			// Apply the created .cfg to every router
+			applycfg.ApplyCfg(router.Name[1:], "big-nw")
+		}
 	}
-
-	for _, router := range as112.Routers {
-		applycfg.ApplyCfg(router.Name[1:], "GNS-project")
-	}
-
-	fmt.Println("-- All .cfg applied.")
 }
